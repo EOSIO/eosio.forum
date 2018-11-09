e8cac26f0559c6ba2772c0d954c4ce25a6046640
Matthieu Vachon <matt@eoscanada.com>
2018-11-09 15:48:01 -0500

    Revert back to "valid" freeze period
    The freeze period in the code was left at the testing value. This was not
    in the contract has the current was deployed much earlier than when this
    changed was introduced.

--- include/forum.hpp
+++ include/forum.hpp
@@ -64,7 +64,7 @@ class forum : public eosio::contract {
 
     private:
         // 3 days in seconds (Computation: 3 days * 24 hours * 60 minutes * 60 seconds)
-        constexpr static uint32_t FREEZE_PERIOD_IN_SECONDS = 2; // NEVER MERGE LIKE THIS
+        constexpr static uint32_t FREEZE_PERIOD_IN_SECONDS = 3 * 24 * 60 * 60;
 
         // 6 months in seconds (Computatio: 6 months * average days per month * 24 hours * 60 minutes * 60 seconds)
         constexpr static uint32_t SIX_MONTHS_IN_SECONDS = (uint32_t) (6 * (365.25 / 12) * 24 * 60 * 60);
